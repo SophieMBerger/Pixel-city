0f9412c9368d38209da0fb2a2f75a5f36056db10
SophieMBerger <berger.m.sophie@gmail.com>
2018-12-25 12:15:54 +0100

    Initial commit

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  pixel-city
+//
+//  Created by Sophie Berger on 25.12.18.
+//  Copyright © 2018 SophieMBerger. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
